8de2c421a80d07c4ea0d93798f32c5ecbc46d9b9
t-sin <shinichi.tanaka45@gmail.com>
2021-04-15 08:11:42 +0900

    Test parsing executable/literal names

--- sources/forth_modoki/03_parser/parser.c
+++ sources/forth_modoki/03_parser/parser.c
@@ -153,10 +153,44 @@ static void test_parse_one_empty_should_return_END_OF_FILE() {
     assert(token.ltype == expect);
 }
 
+static void test_parse_one_executable_name() {
+    char* input = "add";
+    char* expect_name = "add";
+    int expect_type = EXECUTABLE_NAME;
+
+    struct Token token = {UNKNOWN, {0}};
+    int ch;
+
+    cl_getc_set_src(input);
+    ch = parse_one(EOF, &token);
+
+    assert(ch == EOF);
+    assert(token.ltype == expect_type);
+    assert(strcmp(token.u.name, expect_name));
+}
+
+static void test_parse_one_literal_name() {
+    char* input = "/add";
+    char* expect_name = "add";
+    int expect_type = LITERAL_NAME;
+
+    struct Token token = {UNKNOWN, {0}};
+    int ch;
+
+    cl_getc_set_src(input);
+    ch = parse_one(EOF, &token);
+
+    assert(ch == EOF);
+    assert(token.ltype == expect_type);
+    assert(strcmp(token.u.name, expect_name));
+}
 
 static void unit_tests() {
     test_parse_one_empty_should_return_END_OF_FILE();
     test_parse_one_number();
+
+    test_parse_one_executable_name();
+    test_parse_one_literal_name();
 }
 
 int main() {

7d878926c27181a0e9a54c807db468dbb3d7e9ea
t-sin <shinichi.tanaka45@gmail.com>
2021-04-14 19:44:23 +0900

    Define token types and parser function

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -5,6 +5,15 @@
 cc cl_getc.c int_parser_getc.c
 */
 
+typedef enum token_type {
+    SPACE,
+    NUMBER
+} token_type;
+
+int parse_one(int prev_c, token_type *out_type, int *out_val) {
+    return '\0';
+}
+
 int main() {
     int answer1 = 0;
     int answer2 = 0;

3af2fb1056b66f1ef0bdc9b03f3b0a74c43c33c2
t-sin <shinichi.tanaka45@gmail.com>
2021-04-14 21:25:59 +0900

    Can set source string for cl_getc()

--- sources/forth_modoki/02_int_parser_getc/cl_getc.c
+++ sources/forth_modoki/02_int_parser_getc/cl_getc.c
@@ -1,7 +1,7 @@
 #include "clesson.h"
 #include <string.h>
 
-static const char* const input = "123 456";
+static char* input = "123 456";
 static int pos = 0;
 
 
@@ -9,4 +9,9 @@ int cl_getc() {
     if(strlen(input) == pos)
         return EOF;
     return input[pos++];
+}
+
+void cl_getc_set_src(char* str) {
+    input = str;
+    pos = 0;
 }

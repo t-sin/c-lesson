fc36274af18e8f086c39784f532472e849d6a9c7
t-sin <shinichi.tanaka45@gmail.com>
2021-04-15 09:15:06 +0900

    Copy lexical token related code; it will be stack entries

---
+++ sources/forth_modoki/04_stack/token.h
@@ -0,0 +1,23 @@
+/* 03_parserからもってきた */
+
+enum LexicalType {
+    NUMBER,
+    SPACE,
+    EXECUTABLE_NAME,
+    LITERAL_NAME,
+    OPEN_CURLY,
+    CLOSE_CURLY, 
+    END_OF_FILE,
+    UNKNOWN
+};
+
+typedef struct Token {
+    enum LexicalType ltype;
+    union {
+        int number;
+        char onechar;
+        char *name;
+    } u;
+} Token;
+
+#define NAME_SIZE 256

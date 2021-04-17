afd4491eeb457b73beb1d89ac36c99b5045b9d36
t-sin <shinichi.tanaka45@gmail.com>
2021-04-17 11:22:08 +0900

    Copy stack from 04_stack and some fixes
    fixses:
    - include `parser.h` instead of `token.h`
    - hide actual data structure for stack

---
+++ sources/forth_modoki/05_eval_num/stack.c
@@ -0,0 +1,147 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+#define STACK_SIZE 1024
+
+struct Stack {
+    Token *array[STACK_SIZE];
+    int top;
+};
+
+
+Stack* stack_initialize() {
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    stack->top = 0;
+
+    return stack;
+}
+
+int stack_push(Stack *stack, Token *token) {
+    if (stack->top < STACK_SIZE) {
+        stack->array[stack->top] = token;
+        stack->top++;
+        return stack->top;
+    }
+
+    return STACK_FULL;
+}
+
+int stack_pop(Stack *stack, Token **out_token) {
+    if (stack->top == 0) {
+        return STACK_EMPTY;
+    }
+
+    stack->top--;
+    *out_token = stack->array[stack->top];
+
+    return stack->top;
+}
+
+void test_initialize_stack() {
+    int expected_top = 0;
+
+    Stack *stack = stack_initialize();
+
+    assert(stack->top == expected_top);
+}
+
+void test_pop_empty_stack() {
+    int expected_return = STACK_EMPTY;
+    int expected_top = 0;
+    int actual_return;
+    Token* token;
+
+    Stack *stack = stack_initialize();
+    actual_return = stack_pop(stack, &token);
+
+    assert(actual_return == expected_return);
+    assert(stack->top == expected_top);
+}
+
+void test_push_one_integer() {
+    int expected_return = 1;
+    int expected_top = 1;
+    int expected_ltype = NUMBER;
+    int expected_value = 42;
+    int actual_return;
+
+    Token token = {NUMBER, {42}};
+    Stack *stack = stack_initialize();
+
+    actual_return = stack_push(stack, &token);
+
+    // 返り値とトップの確認
+    assert(actual_return == expected_return);
+    assert(stack->top == expected_top);
+    // 値の確認
+    assert(stack->array[0]->ltype == expected_ltype);
+    assert(stack->array[0]->u.number == expected_value);
+}
+
+void test_pop_one_integer() {
+    int expected_return;
+    int expected_top;
+    int expected_ltype = NUMBER;
+    int expected_value = 42;
+    int actual_return;
+
+    Token token = {NUMBER, {42}};
+    Token *out_token;
+    Stack *stack = stack_initialize();
+
+    actual_return = stack_push(stack, &token);
+
+    // 返り値とトップの確認
+    expected_return = 1;
+    expected_top = 1;
+    assert(actual_return == expected_return);
+    assert(stack->top == expected_top);
+
+    actual_return = stack_pop(stack, &out_token);
+
+    // 返り値とトップの確認
+    expected_return = 0;
+    expected_top = 0;
+    assert(actual_return == expected_return);
+    assert(stack->top == expected_top);
+    // 値の確認
+    assert(out_token->ltype == expected_ltype);
+    assert(out_token->u.number == expected_value);
+}
+
+void test_push_to_full_stack() {
+    int expected_return = STACK_FULL;
+    int expected_top = STACK_SIZE;
+    int actual_return;
+
+    Stack *stack = stack_initialize();
+    Token token = {NUMBER, {42}};
+
+    for (int i = 0; i <= STACK_SIZE - 1; i++) {
+        actual_return = stack_push(stack, &token);
+    }
+    assert(actual_return == STACK_SIZE);
+    assert(stack->top == STACK_SIZE);
+
+    // スタックがすべて埋まった状態で要素をプッシュ
+    actual_return = stack_push(stack, &token);
+    assert(actual_return == expected_return);
+    assert(stack->top == expected_top);
+}
+
+void test_all() {
+    test_pop_empty_stack();
+    test_push_one_integer();
+    test_pop_one_integer();
+    test_push_to_full_stack();
+}
+
+#if 0
+int main() {
+    test_all();
+
+    return 0;
+}
+#endif
---
+++ sources/forth_modoki/05_eval_num/stack.h
@@ -0,0 +1,17 @@
+#include "parser.h"
+
+typedef struct Stack Stack;
+
+#define STACK_FULL -1
+#define STACK_EMPTY -2
+
+// スタックを初期化して返す。
+Stack* stack_initialize();
+
+// スタックに要素を追加する。
+// 返り値はtopの位置。スタックがいっぱいの状態でプッシュするとSTACK_FULLが返る。
+int stack_push(Stack *stack, Token *token);
+
+// スタックから要素を取り出す。
+// 返り値はスタックトップの位置。スタックが空の状態でポップするとSTACK_EMPTYが返る。
+int stack_pop(Stack *stack, Token **out_token);

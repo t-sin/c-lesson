6bad624f4d21f188aa13a016093750645592e61a
t-sin <shinichi.tanaka45@gmail.com>
2021-05-10 00:01:20 +0900

    Dictionaries with hash function

--- sources/forth_modoki/interpreter/dict.c
+++ sources/forth_modoki/interpreter/dict.c
@@ -40,40 +40,50 @@ Dict* dict_init() {
     return dict;
 }
 
-#define NOT_FOUND -1
-void dict_put(char *key, Token *token) {
-    int idx = hash(key);
-    DictEntry *head = dict_array[idx];
-
-    if (head == NULL) {
-    }
-
-    int entry_pos = NOT_FOUND;
-    for (int pos = 0; pos < dict_pos; pos++) {
-        if (streq(dict_array[pos].key, key)) {
-            entry_pos = pos;
-            break;
-        }
-    }
-
-    int pos;
-    if (entry_pos == NOT_FOUND) {
-        pos = dict_pos;
-        dict_pos++;
-     } else {
-        pos = entry_pos;
-    }
-
-    //fprintf(stderr, "%ld\n", strlen(key));
-    dict_array[pos].key = (char *)malloc(sizeof(char) * strlen(key));
-    strcpy(dict_array[pos].key, key);
-    token_copy(&dict_array[pos].value, token);
+DictEntry* new_dict_entry(char *key, Token *value) {
+    DictEntry *entry = (DictEntry *)malloc(sizeof(DictEntry));
+    entry->next = NULL;
+    entry->key = (char *)malloc(sizeof(strlen(key) + 1));
+    strcpy(entry->key, key);
+    token_copy(&entry->value, value);
+    return entry;
 }
 
-int dict_get(char *key, Token *out_token) {
-    for (int i = 0; i < dict_pos; i++) {
-        if (streq(dict_array[i].key, key)) {
-            token_copy(out_token, &dict_array[i].value);
+void update_or_insert_list(DictEntry *head, char *key, Token *value) {
+    while (1) {
+        if (streq(head->key, key)) {
+            token_copy(&head->value, value);
+            return;
+        }
+        if (head->next == NULL) {
+            head->next = new_dict_entry(key, value);
+            return;
+        }
+
+        head = head->next;
+    }
+}
+
+void dict_put(Dict *dict, char *key, Token *token) {
+    int idx = hash(key);
+    DictEntry *head = dict->array[idx];
+
+    if (head == NULL) {
+        head = new_dict_entry(key, token);
+        dict->array[idx] = head;
+        return;
+    }
+
+    update_or_insert_list(head, key, token);
+}
+
+int dict_get(Dict *dict, char *key, Token *out_token) {
+    int idx = hash(key);
+    DictEntry *head = dict->array[idx];
+
+    for (; head != NULL; head = head->next) {
+        if (streq(head->key, key)) {
+            token_copy(out_token, &head->value);
             return DICT_FOUND;
         }
     }
@@ -81,92 +91,94 @@ int dict_get(char *key, Token *out_token) {
     return DICT_NOT_FOUND;
 }
 
-void dict_print_all() {
-    printf("[dict] dict_pos = %d\n", dict_pos);
-    for (int i = 0; i < dict_pos; i++) {
-        printf("[dict] %s: ", dict_array[i].key);
-        print_token(&dict_array[i].value);
+void dict_print_all(Dict *dict) {
+    for (int i = 0; i < dict->length; i++) {
+        if (dict->array[i] != NULL) {
+            DictEntry *head = dict->array[i];
+            for (; head != NULL; head = head->next) {
+                printf("[dict] %s: ", head->key);
+                print_token(&head->value);
+            }
+        }
     }
 }
 
 static void test_dict_put_one_integer() {
     Token input = {NUMBER, {42}};
-    char *key = "test";
-    int expected_dict_pos = 1;
+    char *key = "a";
 
-    dict_reset();
-    dict_put(key, &input);
+    Dict *dict = dict_init();
+    dict_put(dict, key, &input);
 
-    assert(dict_pos == expected_dict_pos);
-    assert(dict_array[0].value.ltype == input.ltype);
-    assert(dict_array[0].value.u.number == input.u.number);
+    int idx = hash(key);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input.ltype);
+    assert(dict->array[idx]->value.u.number == input.u.number);
 }
 
 static void test_dict_put_one_literal_name() {
     Token input = {LITERAL_NAME, { .name = "foo"}};
     char *key = "aaa";
-    int expected_dict_pos = 1;
 
-    dict_reset();
-    dict_put(key, &input);
+    Dict *dict = dict_init();
+    dict_put(dict, key, &input);
 
-    assert(dict_pos == expected_dict_pos);
-    assert(dict_array[0].value.ltype == input.ltype);
-    assert(dict_array[0].value.u.name == input.u.name);
+    int idx = hash(key);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input.ltype);
+    assert(dict->array[idx]->value.u.name == input.u.name);
 }
 
-static void test_dict_put_two_different_names() {
+static void test_dict_put_two_different_but_same_hash_names() {
     Token input1 = {LITERAL_NAME, { .name = "foo"}};
     Token input2 = {EXECUTABLE_NAME, { .name = "bar"}};
-    int expected_dict_pos1 = 1;
-    int expected_dict_pos2 = 2;
 
-    dict_reset();
+    Dict *dict = dict_init();
+    int idx;
 
-    dict_put(input1.u.name, &input1);
-    assert(dict_pos == expected_dict_pos1);
-    assert(dict_array[expected_dict_pos1 - 1].value.ltype == input1.ltype);
-    assert(dict_array[expected_dict_pos1 - 1].value.u.name == input1.u.name);
+    dict_put(dict, input1.u.name, &input1);
+    idx = hash(input1.u.name);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input1.ltype);
+    assert(dict->array[idx]->value.u.name == input1.u.name);
 
-    dict_put(input2.u.name, &input2);
-    assert(dict_pos == expected_dict_pos2);
-    assert(dict_array[expected_dict_pos2 - 1].value.ltype == input2.ltype);
-    assert(dict_array[expected_dict_pos2 - 1].value.u.name == input2.u.name);
+    dict_put(dict, input2.u.name, &input2);
+    idx = hash(input2.u.name);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input2.ltype);
+    assert(dict->array[idx]->value.u.name == input2.u.name);
 }
 
 static void test_dict_put_two_same_names() {
     Token input1 = {LITERAL_NAME, { .name = "foo"}};
     Token input2 = {EXECUTABLE_NAME, { .name = "foo"}};
-    int expected_dict_pos1 = 1;
-    int expected_dict_pos2 = 1;
 
-    dict_reset();
+    Dict *dict = dict_init();
+    int idx = hash(input1.u.name);
 
-    dict_put(input1.u.name, &input1);
-    assert(dict_pos == expected_dict_pos1);
-    assert(dict_array[expected_dict_pos1 - 1].value.ltype == input1.ltype);
-    assert(dict_array[expected_dict_pos1 - 1].value.u.name == input1.u.name);
+    dict_put(dict, input1.u.name, &input1);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input1.ltype);
+    assert(dict->array[idx]->value.u.name == input1.u.name);
 
-    dict_put(input2.u.name, &input2);
-    assert(dict_pos == expected_dict_pos2);
-    assert(dict_array[expected_dict_pos2 - 1].value.ltype == input2.ltype);
-    assert(dict_array[expected_dict_pos2 - 1].value.u.name == input2.u.name);
+    dict_put(dict, input2.u.name, &input2);
+    assert(dict->array[idx] != NULL);
+    assert(dict->array[idx]->value.ltype == input2.ltype);
+    assert(dict->array[idx]->value.u.name == input2.u.name);
 }
 
 static void test_dict_get_one_integer() {
     Token input = {NUMBER, {42}};
     int expected_ret = DICT_FOUND;
-    int expected_dict_pos = 1;
 
     char *key = "test";
     Token output;
 
-    dict_reset();
-    dict_put(key, &input);
+    Dict *dict = dict_init();
+    dict_put(dict, key, &input);
 
-    int ret = dict_get(key, &output);
+    int ret = dict_get(dict, key, &output);
     assert(ret == expected_ret);
-    assert(dict_pos == expected_dict_pos);
     assert(output.ltype == input.ltype);
     assert(output.u.number == input.u.number);
 }
@@ -174,16 +186,14 @@ static void test_dict_get_one_integer() {
 static void test_dict_get_one_literal_name() {
     Token input = {LITERAL_NAME, { .name = "name" }};
     int expected_ret = DICT_FOUND;
-    int expected_dict_pos = 1;
 
     Token output;
 
-    dict_reset();
-    dict_put(input.u.name, &input);
+    Dict *dict = dict_init();
+    dict_put(dict, input.u.name, &input);
 
-    int ret = dict_get(input.u.name, &output);
+    int ret = dict_get(dict, input.u.name, &output);
     assert(ret == expected_ret);
-    assert(dict_pos == expected_dict_pos);
     assert(output.ltype == input.ltype);
     assert(streq(output.u.name, input.u.name));
 }
@@ -197,11 +207,11 @@ static void test_dict_get_one_overwritten_integer() {
 
     Token output;
 
-    dict_reset();
-    dict_put(key, &input1);
-    dict_put(key, &input2);
+    Dict *dict = dict_init();
+    dict_put(dict, key, &input1);
+    dict_put(dict, key, &input2);
 
-    int ret = dict_get(key, &output);
+    int ret = dict_get(dict, key, &output);
     assert(ret == expected_ret);
     assert(output.ltype == expected_token->ltype);
     assert(output.u.number == expected_token->u.number);
@@ -212,7 +222,7 @@ int main() {
 
     test_dict_put_one_integer();
     test_dict_put_one_literal_name();
-    test_dict_put_two_different_names();
+    test_dict_put_two_different_but_same_hash_names();
     test_dict_put_two_same_names();
     test_dict_get_one_integer();
     test_dict_get_one_literal_name();
--- sources/forth_modoki/interpreter/dict.h
+++ sources/forth_modoki/interpreter/dict.h
@@ -6,6 +6,6 @@ typedef struct Dict Dict;
 #define DICT_NOT_FOUND 0
 
 void dict_reset();
-void dict_put(char *key, Token *token);
-int dict_get(char *key, Token *out_token);
-void dict_print_all();
+void dict_put(Dict *dict, char *key, Token *token);
+int dict_get(Dict *dict, char *key, Token *out_token);
+void dict_print_all(Dict *dict);

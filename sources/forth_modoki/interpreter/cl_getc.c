049e1e7ded17b9df0ba3c3aa8c8549cb2723259e
t-sin <shinichi.tanaka45@gmail.com>
2021-07-15 21:19:54 +0900

    Avoid `pos`, it is a static variable. And initialize buffer

--- sources/forth_modoki/interpreter/cl_getc.c
+++ sources/forth_modoki/interpreter/cl_getc.c
@@ -23,12 +23,14 @@ void cl_getc_set_src(char* str){
 
 void cl_getc_set_file(FILE *fin){
     char *buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-    int ch, pos;
+    memset(buf, 0, sizeof(char) * BUFFER_SIZE);
+
+    int ch;
+    int idx = 0;
 
     while (ch = fgetc(fin), ch != EOF) {
-        buf[pos++] = ch;
+        buf[idx++] = ch;
     }
-    buf[pos] = '\0';
 
     input = buf;
     len = strlen(buf);
